e8ff9d65595b67bd88a929b2f8a44e3d7187c2bc
Ken Böckler <estken@gmail.com>
2023-10-23 21:45:41 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@ function toggleMenu() {
     menu.classList.toggle('visible');
 }
 
-
+// Hamburger menüü
+document.querySelector('.hamburger').addEventListener('click', function() {
+    var ul = document.querySelector('.header-index nav ul');
+    if (ul.style.display === 'none' || ul.style.display === '') {
+        ul.style.display = 'block';
+    } else {
+        ul.style.display = 'none';
+    }
+});
